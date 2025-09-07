8f3dacac8366d6e3ab082302727d14dad1247628
exquisapp <exqeesolutions@gmail.com>
2025-09-07 17:50:25 -0500

    fix critical CVE vulnerability issues

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
+require('dotenv').config();
+const http = require('http');
 const db = require('./helpers/db');
 const orm = require('./helpers/orm');
-
 const app = require('./app');
+
+const PORT = process.env.PORT || 3000;
+
+if (!process.env.MONGODB) {
+    console.error('Error: MONGODB environment variable is not set.');
+    process.exit(1);
+}
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT, () => {
-    console.log(`Server runs on port ${process.env.PORT}`);
+server.listen(PORT, () => {
+    console.log(`Server runs on port ${PORT}`);
 });
